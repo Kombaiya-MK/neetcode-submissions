98b89d86dfd9303967ca4105f861d3186c4fcf74
Kombaiya M <126459570+Kombaiya-MK@users.noreply.github.com>
2026-06-22 13:31:02 +0530

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    # Bayer - Moore Algorithm
+    def majorityElement(self, nums: List[int]) -> int:
+            candidate = nums[0]
+            count = 1
+
+            for i in range(1, len(nums)):
+                if candidate == nums[i]:
+                    count += 1
+                else:
+                    if count - 1 == 0:
+                        candidate = nums[i]
+                        count = 1
+                    else:
+                        count -= 1
+            return candidate
+        
