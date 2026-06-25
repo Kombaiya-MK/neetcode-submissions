93ee1399b3e402bdb01e4a10476c690d7af1a6a7
Kombaiya M <126459570+Kombaiya-MK@users.noreply.github.com>
2026-06-25 17:17:56 +0530

    Add: lemonade-change - submission-3

---
+++ Data Structures & Algorithms/lemonade-change/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        currentChange = 0
+        changeToGive = 0
+
+        for bill in bills:
+            changeToGive = bill - 5
+            currentChange -= changeToGive
+            # print(currentChange)
+
+            if (currentChange) < 0:
+                return False
+
+            currentChange += 5
+            
+        return True
+
+        
