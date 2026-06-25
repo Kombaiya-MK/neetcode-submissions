44b414a37dc0835f75bcc2be34d5ad89be6ddc24
Kombaiya M <126459570+Kombaiya-MK@users.noreply.github.com>
2026-06-25 13:13:15 +0530

    Add: design-hashmap - submission-2

---
+++ Data Structures & Algorithms/design-hashmap/submission-2.py
@@ -0,0 +1,37 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.size = 1000
+        self.bucket = [[] for _ in range(self.size)]
+
+    def _hash(self, key):
+        return key % self.size
+
+    def put(self, key, value):
+        idx = self._hash(key)
+
+        for pair in self.bucket[idx]:
+            if pair[0] == key:
+                pair[1] = value
+                return
+
+        self.bucket[idx].append([key, value])
+
+    def get(self, key):
+        idx = self._hash(key)
+
+        for k, v in self.bucket[idx]:
+            if k == key:
+                return v
+
+        return -1
+
+    def remove(self, key):
+        idx = self._hash(key)
+
+        bucket = self.bucket[idx]
+
+        for i in range(len(bucket)):
+            if bucket[i][0] == key:
+                bucket.pop(i)
+                return
