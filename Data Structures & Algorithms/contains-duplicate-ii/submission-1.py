314ef45f81415889fce0a515935dad77a4cee9cf
Kombaiya M <126459570+Kombaiya-MK@users.noreply.github.com>
2026-06-29 15:02:47 +0530

    Add: contains-duplicate-ii - submission-1

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        hashMap =  {}
+
+        for idx in range(len(nums)):
+            if nums[idx] in hashMap:
+                diff = idx - hashMap[nums[idx]]
+                if diff <= k:
+                    return True
+                else:
+                    hashMap[nums[idx]] = idx
+            else:
+                hashMap[nums[idx]] = idx
+        return False        
