f1ca22222381cbc4a5e09b22c71d302e56e45042
Kombaiya M <126459570+Kombaiya-MK@users.noreply.github.com>
2026-06-30 16:32:24 +0530

    Add: first-missing-positive - submission-2

---
+++ Data Structures & Algorithms/first-missing-positive/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+
+        idx = 0
+        n = len(nums)
+        while idx < n:
+            currentIdx = nums[idx] - 1
+
+            if 1 <= nums[idx] <= n and nums[idx] != nums[currentIdx]:
+                nums[idx], nums[currentIdx] = nums[currentIdx], nums[idx]
+
+            else:
+                idx += 1        
+        for idx in range(n):
+            if nums[idx] != idx + 1:
+                return idx + 1
+        return n + 1
+
+        # print(nums)
