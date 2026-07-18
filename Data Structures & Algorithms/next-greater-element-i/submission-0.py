86d58b32dbf5bb5439b714d656ebbe5a9ea9490c
Kombaiya M <126459570+Kombaiya-MK@users.noreply.github.com>
2026-07-18 19:45:05 +0530

    Add: next-greater-element-i - submission-0

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        stack = []
+        nextGreater = {}
+
+        for num in nums2:
+
+            while stack and num > stack[-1]:
+                prev = stack.pop()
+                nextGreater[prev] = num
+            stack.append(num)
+
+        return [nextGreater.get(num, -1) for num in nums1]
