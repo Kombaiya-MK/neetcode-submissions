5a1bd116e321625727cc5dabbc5944e02ea0a884
Kombaiya M <126459570+Kombaiya-MK@users.noreply.github.com>
2026-07-30 21:47:26 +0530

    Add: climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/climbing-stairs/submission-5.py
@@ -0,0 +1,9 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dMap = {0:1, 1:1}
+        for i in range(2, n + 1):
+            dMap[i] = dMap[i - 2] + dMap[i-1]
+            print(dMap)
+
+        return dMap[n]
+        
