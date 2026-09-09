a00d8c0810e967b8641f773c7499f37d5a47693c
Kombaiya M <126459570+Kombaiya-MK@users.noreply.github.com>
2026-09-09 21:03:41 +0530

    Add: set-mismatch - submission-1

---
+++ Data Structures & Algorithms/set-mismatch/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        ans = []
+        freq = {}
+        for num in nums:
+            if num in freq:
+                ans.append(num)
+            else:
+                freq[num] = 1
+        for idx in range(1, len(nums) + 1):
+            if idx != nums[idx-1]:
+                ans.append(idx)
+        return ans
+        
